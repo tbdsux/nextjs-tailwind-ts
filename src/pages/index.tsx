cb580225dd3066df5c09a70b3d4db5bd1dfdc6b3
TheBoringDude <iamcoderx@gmail.com>
2021-10-26 08:59:26 +0800

    separate main page codes in modules

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,3 @@
-import Seo from '../components/Seo';
-import DefaultLayout from '../layouts/Default';
+import HomePage from '../modules/home';
 
-export default function Home() {
-  return (
-    <DefaultLayout>
-      <Seo title="Home" />
-
-      <div>start coding!</div>
-    </DefaultLayout>
-  );
-}
+export default HomePage;
