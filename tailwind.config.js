ba700c874bae79ae146f31277440604781ef96ad
TheBoringDude <iamcoderx@gmail.com>
2021-11-06 19:12:17 +0800

    update tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,27 @@
 const colors = require('tailwindcss/colors');
+const defaultTheme = require('tailwindcss/defaultTheme');
 
 module.exports = {
   mode: 'jit',
   // NOTE: configure this if you have another folder with tsx files or (that uses stylinggs)
-  purge: ['./src/pages/**/*.tsx', './src/components/**/*.tsx'],
+  purge: [
+    './src/pages/**/*.tsx',
+    './src/components/**/*.tsx',
+    './src/modules/**/*.tsx',
+    './src/layouts/**/*.tsx'
+  ],
   darkMode: false, // or 'media' or 'class'
   theme: {
     // add all default colors
     colors,
 
     // NOTE: Add the lines below if you want to add a google font
-    // fontFamily: {
-    //   sans: ['"name of google font"', 'sans-serif']
-    // },
+    fontFamily: {
+      sans: [
+        // '"name of google font"', // your google name font
+        ...defaultTheme.fontFamily.sans
+      ]
+    },
     extend: {}
   },
   variants: {
